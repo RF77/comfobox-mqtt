804865925cd02de7aca0532bd94c51a78c8ac04b
RF77 <fendant@outlook.com>
2015-10-26 20:06:34 +0100

    changed to v0.3

--- src/ComfoBoxMqtt/Properties/AssemblyInfo.cs
+++ src/ComfoBoxMqtt/Properties/AssemblyInfo.cs
@@ -46,5 +46,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.2.3.0")]
-[assembly: AssemblyFileVersion("0.2.3.0")]
+[assembly: AssemblyVersion("0.3.0.0")]
+[assembly: AssemblyFileVersion("0.3.0.0")]
