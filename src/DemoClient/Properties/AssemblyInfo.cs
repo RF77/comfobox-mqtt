f7b295cdc17b39af8624a2e8d812886e4775965f
RF77 <fendant@outlook.com>
2015-12-18 23:29:20 +0100

    Version to 0.3.3

--- src/DemoClient/Properties/AssemblyInfo.cs
+++ src/DemoClient/Properties/AssemblyInfo.cs
@@ -63,5 +63,5 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.3.2.0")]
-[assembly: AssemblyFileVersion("0.3.2.0")]
+[assembly: AssemblyVersion("0.3.3.0")]
+[assembly: AssemblyFileVersion("0.3.3.0")]
